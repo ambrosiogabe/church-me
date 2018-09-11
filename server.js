d94cad8e98c313a968c3524ea0524603a61e46b0
Gabe Ambrosio <ambrosiogabe@gmail.com>
2018-09-11 13:01:07 -0500

    Trying to fix server v5

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at ${PORT}/`);
 });
