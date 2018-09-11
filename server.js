f66482e469f8d28915e07a8ac43abe8f2f3350d6
Gabe Ambrosio <ambrosiogabe@gmail.com>
2018-09-11 12:57:36 -0500

    Fixing server v4

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 500;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
+  console.log("Doing it");
   fs.readFile("index.html", function(err, data){
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
