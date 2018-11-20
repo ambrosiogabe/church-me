5f244e4bd7e688de14ccac8d6be4ec6748373326
Gabe Ambrosio <ambrosiogabe@gmail.com>
2018-11-20 11:05:21 -0600

    Trying to save database results temporarily

--- server.js
+++ server.js
@@ -13,40 +13,6 @@ app.use(express.static(__dirname + '/public'));
 // Use heroku's supplied port, default to 3000 for local running
 const port = process.env.PORT || 3000;
 
-all_churches = []
-featured = []
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-client.connect();
-
-client.query('SELECT * from church;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    all_churches.push(row);
-    console.log(JSON.stringify(row));
-  }
-});
-
-client.query('SELECT * from featured_churches', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    var church_id = row.church_id;
-    for (church in all_churches) {
-      console.log("Current row: " + church_id + " Church Id: " + church.church_id);
-      if (church.church_id == church_id) {
-        featured.push(church);
-        console.log(featured);
-        break;
-      }
-    }
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
-
 // Declare the 'controllers'
 
 // index
@@ -58,11 +24,46 @@ app.get('/', function(req, res, next) {
 
 // testing
 app.get('/testing', function(req, res, next) {
+  all_churches = []
+  featured = []
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  });
+
+  client.connect();
+
+  client.query('SELECT * from church;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      all_churches.push(row);
+      console.log(JSON.stringify(row));
+    }
+  });
+
+  client.query('SELECT * from featured_churches', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      var church_id = row.church_id;
+      for (church in all_churches) {
+        console.log("Current row: " + church_id + " Church Id: " + church.church_id);
+        if (church.church_id == church_id) {
+          featured.push(church);
+          console.log(featured);
+          break;
+        }
+      }
+      console.log(JSON.stringify(row));
+    }
+  });
+
   console.log("Featured Churches");
   console.log(featured);
   res.render("index", {
     featured_churches: featured
   });
+
+  client.end();
 });
 
 
