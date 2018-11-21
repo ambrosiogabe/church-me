dd880c890637f01a9d7dcde40e17428cfa6859b5
Gabe Ambrosio <ambrosiogabe@gmail.com>
2018-11-21 10:10:11 -0600

    Adding church adding form

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.post('/add_church', function(req, web_res, next) {
   var query_string = "insert into church values ('" + req.body.church.state + "', '" + req.body.church.zip + "', '" + req.body.church.city + "', '" + req.body.church.name + "', '" + req.body.church.long + "', '" + req.body.church.lat + "', '" + req.body.church.video_link + "', '" + req.body.church.image_path + "');";
   client.connect();
 
-  client.query("select (select count(*) from church) as id;" (err, count_res) => {
+  client.query("select (select count(*) from church) as id;", (err, count_res) => {
     if(err) throw err;
     var count = count_res[0].id;
     var query_string = "insert into church values (" + count + ", '" + req.body.church.state + "', '" + req.body.church.zip + "', '" + req.body.church.city + "', '" + req.body.church.name + "', '" + req.body.church.long + "', '" + req.body.church.lat + "', '" + req.body.church.video_link + "', '" + req.body.church.image_path + "');";
